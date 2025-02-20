c8782ca75051ccc98abc3d5754f667c550557b11
Victor Tesoura Júnior <txsoura@yahoo.com>
2025-02-20 13:49:51 -0300

    first commit

---
+++ artisan.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import { Exec } from '@athenna/common'
+
+Exec.artisan('./bin/artisan.js', {
+  nodeOptions: [
+    '--enable-source-maps',
+    '--disable-warning=DEP0180',
+    '--disable-warning=DEP0040',
+    '--import=@athenna/tsconfig'
+  ]
+})
---
+++ tests/e2e/console/commands/app.command.test.ts
@@ -0,0 +1,14 @@
+import { Test, type Context } from '@athenna/test'
+import { BaseConsoleTest } from '@athenna/core/testing/BaseConsoleTest'
+
+export default class AppCommandTest extends BaseConsoleTest {
+  @Test()
+  public async shouldBeAbleToGetTheStdoutOfAppCommand({ command }: Context) {
+    const output = await command.run('app')
+
+    output.assertSucceeded()
+    output.assertLogged(`Application information's:`)
+    output.assertLogged('@athenna/athenna')
+    output.assertLogged('Athenna is awesome!')
+  }
+}
