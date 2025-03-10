c96b8d5a62c2151e3fe7c3ae69d967c1cad18cbc
Victor Tesoura Júnior <txsoura@yahoo.com>
2025-03-10 16:15:28 -0300

    feat: create show command

---
+++ src/console/commands/show.command.ts
@@ -0,0 +1,36 @@
+import { Argument, BaseCommand } from '@athenna/artisan'
+import { AppService } from '#src/services/app.service'
+
+export class CreateCommand extends BaseCommand {
+  @Argument({
+    required: true,
+    description: 'The ssh key name'
+  })
+  private name: string
+
+  public static signature(): string {
+    return 'show'
+  }
+
+  public static description(): string {
+    return 'Show specific SSH public key by name.'
+  }
+
+  public async handle(): Promise<void> {
+    const service = new AppService()
+
+    const publicKey = await service.getOne(this.name)
+
+    if (!publicKey) {
+      this.logger.error('The key name does not exist')
+
+      return
+    }
+
+    this.logger.simple(`[ ({bold,green} ${this.name} public key) ]`)
+
+    this.logger.simple(publicKey)
+
+    this.logger.simple('[ ============================================== ]')
+  }
+}
