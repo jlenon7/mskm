91804a839ce8942725deebced01686b5279d57bb
Victor Tesoura Júnior <txsoura@yahoo.com>
2025-03-10 16:15:16 -0300

    feat: create delete command

---
+++ src/console/commands/delete.command.ts
@@ -0,0 +1,34 @@
+import { Argument, BaseCommand } from '@athenna/artisan'
+import { AppService } from '#src/services/app.service'
+
+export class CreateCommand extends BaseCommand {
+  @Argument({
+    required: true,
+    description: 'The ssh key name'
+  })
+  private name: string
+
+  public static signature(): string {
+    return 'delete'
+  }
+
+  public static description(): string {
+    return 'Delete specific SSH key by name.'
+  }
+
+  public async handle(): Promise<void> {
+    const service = new AppService()
+
+    this.logger.simple('[ Deleting SSH key ]')
+
+    if (!(await service.findPair(this.name))) {
+      this.logger.error('The key name does not exist')
+
+      return
+    }
+
+    await service.delete(this.name)
+
+    this.logger.success(`Successfully deleted ({bold,yellow} ${this.name}) key`)
+  }
+}
