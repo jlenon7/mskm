e597b13f7e74eda4c52a35047a013194c72e36b6
Victor Tesoura Júnior <txsoura@yahoo.com>
2025-03-10 16:15:41 -0300

    feat: create use command

---
+++ src/console/commands/use.command.ts
@@ -0,0 +1,41 @@
+import { Argument, BaseCommand } from '@athenna/artisan'
+import { AppService } from '#src/services/app.service'
+import { File, Path } from '@athenna/common'
+
+export class UseCommand extends BaseCommand {
+  @Argument({
+    required: true,
+    description: 'The ssh key name'
+  })
+  private name: string
+
+  public static signature(): string {
+    return 'use'
+  }
+
+  public static description(): string {
+    return 'Set specific SSH key as default by name.'
+  }
+
+  public async handle(): Promise<void> {
+    const service = new AppService()
+
+    if (!(await service.findPair(this.name))) {
+      this.logger.error('The key name does not exist')
+
+      return
+    }
+
+    if (await File.exists(Path.bin(service.filename))) {
+      await service.setFileContent(this.name)
+    } else {
+      await service.createFileWithContent(this.name)
+    }
+
+    await service.changeCurrentSSHKey(this.name)
+
+    this.logger.success(
+      `({bold,white} ${this.name}) is now the default SSH key`
+    )
+  }
+}
